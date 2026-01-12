89c3b92827477535ce50f91d99a671fdbbc3c83c
AshtonW <36992236+wolfman1986@users.noreply.github.com>
2026-01-11 17:01:12 -0800

    Update visitors-handler.js

--- api/shared/visitors-handler.js
+++ api/shared/visitors-handler.js
@@ -1,1 +1,11 @@
+let count = 0;
+
+module.exports = async function (context, req) {
+  count += 1;
+
+  context.res = {
+    headers: { "Content-Type": "application/json" },
+    body: { count }
+  };
+};
 
